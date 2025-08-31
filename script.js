6b4a114c0fbed6e06770afa370075a0cb9f39438
dianannperla <waafirmelyza@hotmail.com>
2025-08-31 15:57:16 +0100

    Updated background color to improve contrast

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('The webpage is ready!');
-    document.body.style.backgroundColor = '#f0f0f0';
+    document.body.style.backgroundColor = '#ffffff';
 });
